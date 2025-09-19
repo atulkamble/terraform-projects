94d496a06d3936248add5862bd8e80dc7756ac2b
Atul Kamble <atul_kamble@hotmail.com>
2025-09-19 12:05:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.3.0"
+      version = "6.14.0"
     }
   }
 }
@@ -12,10 +12,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
-  ami           = "ami-0150ccaf51ab55a51"
+  ami           = "ami-08982f1c5bf93d976"
   instance_type = "t3.micro"
+  count = "3"
 
   tags = {
-    Name = "one"
+    Name = "MyInstance"
   }
 }
